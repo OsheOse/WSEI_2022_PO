7b6a6a454a2f956b58630077af3603fd94279e19
KrystianKuska <47749622+OsheOse@users.noreply.github.com>
2022-05-29 07:36:14 +0200

    drawing game board

--- WSEI_2022_PO_Krystian_Kuska/MainWindow.xaml.cs
+++ WSEI_2022_PO_Krystian_Kuska/MainWindow.xaml.cs
@@ -20,9 +20,47 @@ namespace WSEI_2022_PO_Krystian_Kuska
     /// </summary>
     public partial class MainWindow : Window
     {
+        const int squareSize = 20;
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void Window_ContentRenderer(object sender, EventArgs e)
+        {
+            DrawArea();
+        }
+        private void DrawArea()
+        {
+            bool doneDrawingBackground = false;
+            int nextX = 0, nextY = 0;
+            int rowCounter = 0;
+            bool nextIsOdd = false;
+
+            while (doneDrawingBackground == false)
+            {
+                Rectangle rect = new Rectangle
+                {
+                    Width = squareSize,
+                    Height = squareSize,
+                    Fill = nextIsOdd ? Brushes.White : Brushes.Black
+                };
+                GameArea.Children.Add(rect);
+                Canvas.SetTop(rect, nextY);
+                Canvas.SetLeft(rect, nextX);
+
+                nextIsOdd = !nextIsOdd;
+                nextX += squareSize;
+                if (nextX >= GameArea.ActualWidth)
+                {
+                    nextX = 0;
+                    nextY += squareSize;
+                    rowCounter++;
+                    nextIsOdd = (rowCounter % 2 != 0);
+                }
+
+                if (nextY >= GameArea.ActualHeight)
+                    doneDrawingBackground = true;
+            }
         }
     }
 }
